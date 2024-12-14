dfe7359c91e1dd85aebeaf57d37d3260289c5c03
anastasia <beloglazova2004@mail.ru>
2024-12-14 15:31:34 +0300

    added about author

---
+++ src/main/java/org/example/tourist/controller/HomeController.java
@@ -0,0 +1,23 @@
+package org.example.tourist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    /**
+     * Отображает страницу "Об авторе".
+     * Страница доступна для всех пользователей.
+     *
+     * @param model модель для представления
+     * @return имя шаблона страницы "about"
+     */
+    @GetMapping("/about")
+    public String aboutPage(Model model) {
+        // Добавляем данные в модель, если нужно
+        model.addAttribute("pageTitle", "Об авторе | YourAgencyName");
+        return "about";
+    }
+}
+
